90e7868b79dc47ddb3b002d2156ea6d86c51fb2d
FlavioACR <flavioabatcarrolar@outlook.com>
2022-06-24 22:54:48 -0500

    Agregando comentarios al archivo hello_word.py y terminando searchtest.py

--- hello_word.py
+++ hello_word.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
+# Realizado con class method para ejecutar el testing en una sola pestaña del navegador:
 class HolaMundo(unittest.TestCase):
     '''Esta clase se divide en 3 partes;
     
@@ -22,9 +23,9 @@ class HolaMundo(unittest.TestCase):
         # Ruta del driver en variable:
         # Optimizamor la variable que controla el driver, para no repetir lo anterior constantemente:
         cls.driver = webdriver.Chrome(service=chrome)
-        # Le decimos al driver que esperde determinado tiempoa antes de hacer otra acción:
+        # Le decimos al driver que esperde determinado tiempo antes de hacer otra acción:
         WebDriverWait(cls.driver, 15)
-
+        
     def test_hola(self):
         # Caso de prueba serie de acciones para automatizar:
         # Declaramos nuevamente la variable driver
--- searchtests.py
+++ searchtests.py
@@ -1,14 +1,13 @@
 # We import the librarys and modules that wee need:
+from ast import Pass
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Si class methos para hacerlo todo en una pestañra
-
-
-class HolaMundo(unittest.TestCase):
+# Todo se ejecutara en una pestaña diferente:
+class searchtst(unittest.TestCase):
     
     def setUp(self):
         # Preparacion del Driver:
@@ -24,12 +23,36 @@ class HolaMundo(unittest.TestCase):
         driver.implicitly_wait(14)
 
     def test_search_texte_field(self):
-        # Define como se realizará la busqueda del elemento:
+        # Define como se realizará la busqueda del elemento
+        # Se realiza busqueda por el nombre del elemento:
         search_field = self.driver.find_element_by_name("q")    
 
     def test_search_texte_field_by_class_name(self):
-        # Define como se realizará la busqueda del elemento:
-        search_field = self.driver.find_element_by_class_name("input-text required-entry")    
+         # Define como se realizará la busqueda del elemento
+         # se realiza busqueda por el nombre de la clase:
+         search_field = self.driver.find_element_by_class_name("input-text")    
+
+    def test_search_button_enabled(self):
+         # Define como se realizará la busqueda del elemento
+         # Verificar si un boton se encuentra disponible:
+         button = self.driver.find_element_by_class_name("button")    
+
+    def test_count_promo_banner_images(self):
+         # Definimos una variable para guardar la lista
+         # en este caso una lista distorcionada:
+         banner_list = self.driver.find_element_by_class_name("promos")    
+         # Definimos una variable en donde se almacenaran
+         # los banners, realizando una busqueda dentro de la lista
+         # similar a usa expresiones Xpath, mediante el nombre del tag:
+         banners = self.driver.find_element_by_tag_name('img')    
+         
+    def test_vip_promo(self):
+        # Identificar con el la ruta Xpath
+        vip_promo = self.driver.find_element_by_xpath('//*[@id="top"]/body/div[1]/div[2]/div[2]/div/div/div[2]/ul/li[1]/a/img')
+
+    def test_shopping_cart(self):
+        shopping_cart_icon = self.driver.find_elements_by_css_selector("div.header-minicart span.icon") 
+
 
     def tearDown(self):
         # Cerrar todo el proceso.
@@ -37,4 +60,4 @@ class HolaMundo(unittest.TestCase):
     
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output="report", report_name="hello_word_report"))
+    unittest.main(verbosity = 2)
